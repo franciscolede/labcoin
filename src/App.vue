e9a32a0a809963be58726c93692734d83c846a0c
franciscolede <franciscolede01@gmail.com>
2023-08-23 00:26:17 -0300

    funcionamiento footer

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
-  <Navbar/>
-  <router-view/>
+  <div id="app">
+    <Navbar />
+    <router-view id="content" />
+    <footer id="footer">
+      asd
+    </footer>
+  </div>
 </template>
 
 <script>
@@ -25,6 +30,10 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: rgb(217, 240, 232);
+
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
 }
 
 body{
@@ -35,6 +44,14 @@ a {
   color: inherit; /* Hereda el color del texto del elemento padre */
   text-decoration: none; /* Elimina el subrayado */
   transition: none; /* Elimina las transiciones */
+}
+
+#content {
+  flex: 1; /* Permite que el contenido ocupe el espacio restante */
+}
+
+#footer {
+  flex-shrink: 0; /* Evita que el footer se contraiga cuando el contenido es pequeño */
 }
 
 </style>
